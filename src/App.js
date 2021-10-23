3fe7fc1c61e279759b6f1043264dfb1560363e01
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-23 18:05:51 +0300

    lists in react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Students } from "./components/Students/Students";
+import { students } from "./api/students";
+
 function App(props) {
   const { course, cohort, instructor, beginner = false} = props;
   const coursePrefix = beginner ? 'Beginner' : 'Advanced';
@@ -5,9 +8,10 @@ function App(props) {
   return (
     <div className="App">
       <article>
-        <h1>Welcome to {coursePrefix} { course }!</h1>
+        <h1>Welcome to { coursePrefix } { course }!</h1>
         <p>This is cohort number { cohort }.</p>
         <p>Taught by instructor { instructor }.</p>
+        <Students title="candidates" list={students}/>
       </article>
     </div>
   );
---
+++ src/components/Students/Students.js
@@ -0,0 +1,16 @@
+import { Student } from "../Student/Student";
+
+export function Students({ title, list = []}) {
+    return (
+        <>
+            <h2>Here is the list of {title}</h2>
+            <div>Name - Profession</div>
+            <ul>
+                {
+                    list.map((student, index) =>
+                        <li><Student {...student}/></li>)
+                }
+            </ul>
+        </>
+    );
+}
