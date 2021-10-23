9df73960b306eda078a2c91e0c8f640b658218c1
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-23 17:34:04 +0300

    props in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-function App() {
+function App(props) {
+  const { course, cohort, instructor, beginner = false} = props;
+  const coursePrefix = beginner ? 'Beginner' : 'Advanced';
+
   return (
     <div className="App">
-      <header className="App-header">
-        Time to learn React!
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article>
+        <h1>Welcome to {coursePrefix} { course }!</h1>
+        <p>This is cohort number { cohort }.</p>
+        <p>Taught by instructor { instructor }.</p>
+      </article>
     </div>
   );
 }
