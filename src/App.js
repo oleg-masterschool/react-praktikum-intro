d5fd959cecd9f45e181c7938a38ae825bb9b5c2b
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-11-01 22:05:51 +0200

    adds json-server + delete endpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-
+import { useHistory } from "react-router-dom";
 // Step 2: import Route, Link Switch from react router dom
 
 
 import './app.css';
-import { students } from './api/data';
+// import { students } from './api/data';
+import cohortData from './api/data01.json';
 import { Link, Route, Switch } from "react-router-dom";
 // Components for routing
 import { Home } from './components/Home';
@@ -13,42 +14,72 @@ import { Student } from './components/Student';
 
 
 // simulates api call
-function fetchStudents() {
-    return Promise.resolve({ success: true, data: students });
+async function fetchStudents() {
+    const response = await fetch('http://localhost:3000/students');
+    return response.json();
+}
+
+function fetchCohort() {
+    return Promise.resolve({ success: true, data: cohortData });
+}
+
+function deleteStudent(id) {
+    return fetch(`http://localhost:3000/students/${id}`, { method: "DELETE" });
 }
 
 function App() {
+    const history = useHistory();
     const [students, setStudents] = useState([]);
+    const [cohort, setCohort] = useState([]);
 
     useEffect(() => {
-        fetchStudents().then(res => setStudents(res.data));
+        fetchStudents().then((data) =>
+            setStudents(data)
+        );
     }, []);
 
-    return (
-        <div className="App">
-            <nav>
-                <h1>Student Registry</h1>
-                <div className="nav-links">
-                    {/*Step 3 Make links to navigate to Home '/', and Student's list '/students-list' */}
-                    <Link to="/">Home</Link>
-                    <Link to="/students-list">Students</Link>
-                </div>
-            </nav>
+    useEffect(() => {
+        async function getCohort() {
+            const res = await fetchCohort();
+            setCohort(res.data.cohort);
+        }
 
-            {/*Step 4 - Build a Switch block with Route for each of the components at the top*/}
-            <Switch>
-                <Route path="/students-list/:studentId">
-                    <Student students={students}/>
-                </Route>
-                <Route path="/students-list">
-                    <List students={students}/>
-                </Route>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-            </Switch>
-        </div>
-    );
+        getCohort();
+    }, []);
+
+async function removeStudent(id) {
+    await deleteStudent(id);
+    const data = await fetchStudents();
+    setStudents(data);
+    history.push('/');
+}
+
+return (
+    <div className="App">
+        <nav>
+            <h1>Student Registry</h1>
+            <h2>Welcome to number {cohort}!</h2>
+            <div className="nav-links">
+                {/*Step 3 Make links to navigate to Home '/', and Student's list '/students-list' */}
+                <Link to="/">Home</Link>
+                <Link to="/students-list">Students</Link>
+            </div>
+        </nav>
+
+        {/*Step 4 - Build a Switch block with Route for each of the components at the top*/}
+        <Switch>
+            <Route path="/students-list/:studentId">
+                <Student cohort={cohort} students={students}/>
+            </Route>
+            <Route path="/students-list">
+                <List students={students} onClick={removeStudent}/>
+            </Route>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+        </Switch>
+    </div>
+);
 }
 
 export { App };
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,19 +4,23 @@ import './index.css';
 // Import a Link and useRouteMatch hook from 'react-router-dom'
 import { Link } from "react-router-dom";
 
-export const List = ({ students }) => {
+export const List = ({ students, onClick }) => {
     // Grab the current URL using the hook
     return (
         <div className="students-list">
             {students.map(student => (
-                <Link to={`/students-list/${student.id}`}>
-                    <div className="student-card" key={student.id}>
-                        {/*STEP 6 - Link starts, navigates from "current_url" to "current_url/student_id"*/}
-                        <img src={process.env.PUBLIC_URL + student.image} alt={student.name}/>
-                        <p>{student.name}</p>
-                        {/*link ends here*/}
-                    </div>
-                </Link>
+                <div>
+                    <Link to={`/students-list/${student.id}`}>
+                        <div className="student-card" key={student.id}>
+                            {/*STEP 6 - Link starts, navigates from "current_url" to "current_url/student_id"*/}
+                            <img src={process.env.PUBLIC_URL + student.image} alt={student.name}/>
+                            <p>{student.name}</p>
+
+                            {/*link ends here*/}
+                        </div>
+                    </Link>
+                    <button onClick={() => onClick(student.id)}>Delete</button>
+                </div>
             ))}
         </div>
     )
